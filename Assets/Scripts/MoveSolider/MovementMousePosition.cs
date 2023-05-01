b3f473495bf18d1aa2ce0c2cba22b3730d1ecfb8
Yusuf <yusuf_zeki1234@hotmail.com>
2023-05-01 13:59:50 +0300

    Player Edit

--- Assets/Scripts/MoveSolider/MovementMousePosition.cs
+++ Assets/Scripts/MoveSolider/MovementMousePosition.cs
@@ -4,39 +4,5 @@ using UnityEngine;
 
 public class MovementMousePosition : MonoBehaviour
 {
-    public Solider player;
-    public Vector3 screenPosition;
-    public Vector3 worldPosition;
-    public LayerMask layersToHit;
 
-    public Collider trackingArea; // takip edilecek bölgenin Collider bileþeni
-
-    private void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<Solider>();
-    }
-
-    private void Update()
-    {
-        screenPosition = Input.mousePosition;
-
-        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-
-        RaycastHit hit;
-        
-        if (Physics.Raycast(ray, out hit) && hit.collider == trackingArea)
-        { 
-            worldPosition = hit.point;
-        }
-
-        if (player.selected == true)
-        {
-            player.transform.position = new Vector3(
-                Mathf.Round(worldPosition.x),
-                1,
-                Mathf.Round(worldPosition.z)
-                );
-        }
-
-    }
 }
--- Assets/Scripts/Solider/Solider.cs
+++ Assets/Scripts/Solider/Solider.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Solider : MonoBehaviour
 {
     public bool selected;
-    public int id;
+    public int level;
 
     public Transform Enemy;
     public NavMeshAgent agent;
+
+    public Vector3 screenPosition;
+    public Vector3 worldPosition;
+    public LayerMask layersToHit;
+
+    public Collider trackingArea;
+    Vector3 position;
+
+    private void Start()
+    {
+        position = transform.position;
+    }
 
     private void OnMouseDown()
     {
@@ -20,14 +33,61 @@ public class Solider : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             selected = false;
+            position = transform.position;
         }
         if (Input.GetKeyDown(KeyCode.F))
         {
             AttackTime();
         }
+        screenPosition = Input.mousePosition;
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider == trackingArea)
+        {
+            worldPosition = hit.point;
+        }
+
+        if (selected == true)
+        {
+            transform.position = new Vector3(
+                Mathf.Round(worldPosition.x),
+                1,
+                Mathf.Round(worldPosition.z)
+                );
+        }
     }
     public void AttackTime()
     {
         agent.destination = Enemy.position;
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            agent.destination = transform.position;
+        }
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (selected == true)
+            {
+                int levelControl = other.gameObject.GetComponent<Solider>().level;
+                if (level == levelControl)
+                {
+                    Destroy(other.gameObject);
+                    transform.localScale = new Vector3(
+                        transform.localScale.x+ 0.05f,
+                        transform.localScale.y+ 0.05f,
+                        transform.localScale.z+ 0.05f
+                        );
+                }
+                else
+                {
+                    transform.position = position;
+                }
+            }
+        }
     }
 }
