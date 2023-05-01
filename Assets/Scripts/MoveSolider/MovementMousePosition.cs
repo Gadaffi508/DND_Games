82d197eda790b854ec326c5c1e8c18be028991ff
Yusuf <yusuf_zeki1234@hotmail.com>
2023-05-01 16:44:37 +0300

    Edited

--- Assets/Scripts/MoveSolider/MovementMousePosition.cs
+++ Assets/Scripts/MoveSolider/MovementMousePosition.cs
@@ -1,8 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class MovementMousePosition : MonoBehaviour
+public abstract class MovementMousePosition : MonoBehaviour
 {
+    public bool selected;
+    public int level;
 
+    public Transform Enemy;
+    public NavMeshAgent agent;
+
+    public Vector3 screenPosition;
+    public Vector3 worldPosition;
+    public LayerMask layersToHit;
+
+    public Collider trackingArea;
+    public Vector3 position;
+
+    private void Start()
+    {
+        position = transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        selected = true;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            selected = false;
+            position = transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AttackTime();
+        }
+        screenPosition = Input.mousePosition;
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider == trackingArea)
+        {
+            worldPosition = hit.point;
+        }
+
+        if (selected == true)
+        {
+            transform.position = new Vector3(
+                Mathf.Round(worldPosition.x),
+                1,
+                Mathf.Round(worldPosition.z)
+                );
+        }
+    }
+    public abstract void AttackTime();
+
+    
 }
--- Assets/Scripts/Solider/Solider.cs
+++ Assets/Scripts/Solider/Solider.cs
@@ -4,62 +4,9 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
 
-public class Solider : MonoBehaviour
+public class Solider : MovementMousePosition
 {
-    public bool selected;
-    public int level;
-
-    public Transform Enemy;
-    public NavMeshAgent agent;
-
-    public Vector3 screenPosition;
-    public Vector3 worldPosition;
-    public LayerMask layersToHit;
-
-    public Collider trackingArea;
-    Vector3 position;
-
-    private void Start()
-    {
-        position = transform.position;
-    }
-
-    private void OnMouseDown()
-    {
-        selected = true;
-    }
-    private void Update()
-    {
-        if (Input.GetMouseButtonUp(0))
-        {
-            selected = false;
-            position = transform.position;
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            AttackTime();
-        }
-        screenPosition = Input.mousePosition;
-
-        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit) && hit.collider == trackingArea)
-        {
-            worldPosition = hit.point;
-        }
-
-        if (selected == true)
-        {
-            transform.position = new Vector3(
-                Mathf.Round(worldPosition.x),
-                1,
-                Mathf.Round(worldPosition.z)
-                );
-        }
-    }
-    public void AttackTime()
+    public override void AttackTime()
     {
         agent.destination = Enemy.position;
     }
@@ -76,16 +23,19 @@ public class Solider : MonoBehaviour
                 int levelControl = other.gameObject.GetComponent<Solider>().level;
                 if (level == levelControl)
                 {
+                    level += 1;
                     Destroy(other.gameObject);
                     transform.localScale = new Vector3(
-                        transform.localScale.x+ 0.05f,
-                        transform.localScale.y+ 0.05f,
-                        transform.localScale.z+ 0.05f
+                        transform.localScale.x + 0.05f,
+                        transform.localScale.y + 0.05f,
+                        transform.localScale.z + 0.05f
                         );
                 }
                 else
                 {
-                    transform.position = position;
+                    Debug.Log("oolmadý");
+                    Vector3 _position = other.gameObject.GetComponent<Solider>().position;
+                    transform.position = _position;
                 }
             }
         }
