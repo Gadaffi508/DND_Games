44b2fac8856cf10462504ea832e0f0321a2b32c7
Yusuf <yusuf_zeki1234@hotmail.com>
2023-08-08 08:41:58 +0300

    enemy level updated

--- Assets/Scripts/Enemy/LevelEnemySystem.cs
+++ Assets/Scripts/Enemy/LevelEnemySystem.cs
@@ -8,6 +8,10 @@ public class LevelEnemySystem : MonoBehaviour
     [SerializeField] private GameObject[] m_ork; 
     [SerializeField] private GameObject[] m_Trol;
 
+    [Space]
+    [Header("EnemyType")]
+    [SerializeField] private GameObject[] m_enemyTypeImage;
+
     private void Start()
     {
         EnemyGameLevelActive();
@@ -19,33 +23,42 @@ public class LevelEnemySystem : MonoBehaviour
         {
             case 1:
                 GameObjectFor(m_cüce,true);
+                EnemyImageShow(0,1,2);
                 break;
             case 2:
                 GameObjectFor(m_ork,true);
+                EnemyImageShow(3, 4, 5);
                 break;
             case 3:
                 GameObjectFor(m_Trol,true);
+                EnemyImageShow(6, 7, 8);
                 break;
             case 4:
                 MixGameObjectFor(0,1,2);
+                EnemyImageShow(2, 4, 6);
                 break;
             case 5:
                 MixGameObjectFor(0,2,1);
+                EnemyImageShow(2, 3, 7);
                 break;
             case 6:
                 MixGameObjectFor(1,2,0);
+                EnemyImageShow(3, 1, 8);
                 break;
             case 7:
                 MixGameObjectFor(1,0,2);
+                EnemyImageShow(5, 1, 6);
                 break;
             case 8:
                 MixGameObjectFor(2,0,1);
+                EnemyImageShow(0, 7, 5);
                 break;
             case 9:
                 MixGameObjectFor(2,1,0);
+                EnemyImageShow(0, 4, 8);
                 break;
             default:
-                GameObjectFor(m_cüce, true);
+                //
                 break;
         }
     }
@@ -63,5 +76,17 @@ public class LevelEnemySystem : MonoBehaviour
         m_cüce[a].SetActive(true);
         m_ork[b].SetActive(true);
         m_Trol[c].SetActive(true);
+    }
+
+    public void EnemyImageShow(int a, int b, int c)
+    {
+        foreach (GameObject ýmageAll in m_enemyTypeImage)
+        {
+            ýmageAll.SetActive(false);
+        }
+
+        m_enemyTypeImage[a].SetActive(true);
+        m_enemyTypeImage[b].SetActive(true);
+        m_enemyTypeImage[c].SetActive(true);
     }
 }
