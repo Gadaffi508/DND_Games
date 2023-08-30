d5543c661ede40c33635a7ba001d75908041e2fc
Yusuf <yusuf_zeki1234@hotmail.com>
2023-08-30 23:24:27 +0300

    ımage upgrade updated

--- Assets/Scripts/StartSceneScripts/LevelManager.cs
+++ Assets/Scripts/StartSceneScripts/LevelManager.cs
@@ -37,24 +37,24 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private int m_fighterLevelGold;
     [SerializeField] private int m_fighterDamage;
     [SerializeField] private int m_fighterHealth;
-    [SerializeField] private Text[] m_fighterHealthText;
-    [SerializeField] private Text[] m_fighterStrengText;
+    [SerializeField] private Image[] m_fighterHealthImage;
+    [SerializeField] private Image[] m_fighterStrengImage;
     [Space]
     [Header("Wizard")]
     [SerializeField] private int m_WizardLevel;
     [SerializeField] private int m_WizardLevelGold;
     [SerializeField] private int m_WizardDamage;
     [SerializeField] private int m_WizardHealth;
-    [SerializeField] private Text[] m_WizardHealthText;
-    [SerializeField] private Text[] m_WizardStrengText;
+    [SerializeField] private Image[] m_WizardHealthImage;
+    [SerializeField] private Image[] m_WizardStrengImage;
     [Space]
     [Header("Thief")]
     [SerializeField] private int m_ThiefLevel;
     [SerializeField] private int m_ThiefLevelGold;
     [SerializeField] private int m_ThiefDamage;
     [SerializeField] private int m_ThiefHealth;
-    [SerializeField] private Text[] m_ThiefHealthText;
-    [SerializeField] private Text[] m_ThiefStrengText;
+    [SerializeField] private Image[] m_ThiefHealthImage;
+    [SerializeField] private Image[] m_ThiefStrengImage;
 
     //Propertys
     bool open = true;
@@ -64,7 +64,7 @@ public class LevelManager : MonoBehaviour
     private void Start()
     {
         GetDice();
-        
+
         for (int i = 0; i < dices.Length; i++)
         {
             dices[i].SetActive(false);
@@ -245,27 +245,27 @@ public class LevelManager : MonoBehaviour
         }
     }
 
-    public void CharactersTextWrite(Text[] charecter_T, int property, string propert_name)
+    public void CharactersTextWrite(Image[] charecter_T, float property, float maxProperty)
     {
-        foreach (Text fighter_t in charecter_T)
+        foreach (Image fighter_t in charecter_T)
         {
-            fighter_t.text = $"{propert_name} : {property}";
+            fighter_t.fillAmount = property / maxProperty;
         }
     }
 
     public void GetCharecterText()
     {
         //-------------------------------------figter--------------------------\\
-        CharactersTextWrite(m_fighterHealthText, m_fighterHealth + (m_fighterLevel * 10), "Health");
-        CharactersTextWrite(m_fighterStrengText, m_fighterDamage + (m_fighterLevel * 10), "Attack Damage");
+        CharactersTextWrite(m_fighterHealthImage, m_fighterHealth + (m_fighterLevel * 10), 300);
+        CharactersTextWrite(m_fighterStrengImage, m_fighterDamage + (m_fighterLevel * 10), 100);
 
         //-------------------------------------Wizard--------------------------\\
-        CharactersTextWrite(m_WizardHealthText, m_WizardHealth + (m_WizardLevel * 10), "Health");
-        CharactersTextWrite(m_WizardStrengText, m_WizardDamage + (m_WizardLevel * 10), "Attack Damage");
+        CharactersTextWrite(m_WizardHealthImage, m_WizardHealth + (m_WizardLevel * 10), 300);
+        CharactersTextWrite(m_WizardStrengImage, m_WizardDamage + (m_WizardLevel * 10), 100);
 
         //-------------------------------------Thief--------------------------\\
-        CharactersTextWrite(m_ThiefHealthText, m_ThiefHealth + (m_ThiefLevel * 10), "Health");
-        CharactersTextWrite(m_ThiefStrengText, m_ThiefDamage + (m_ThiefLevel * 10), "Attack Damage");
+        CharactersTextWrite(m_ThiefHealthImage, m_ThiefHealth + (m_ThiefLevel * 10), 300);
+        CharactersTextWrite(m_ThiefStrengImage, m_ThiefDamage + (m_ThiefLevel * 10), 100);
     }
 
     public void SoundPlay(AudioSource m_buttonClick)
@@ -278,7 +278,7 @@ public class LevelManager : MonoBehaviour
 
     public void MusýcClose()
     {
-        open =! open;
+        open = !open;
         m_closeImage.SetActive(!open);
         if (open)
         {
